55e2bb72dda16863af3259f6eff6fb60253abcb7
Devashishkatiyar <katiyardevashish24@gmail.com>
2020-12-05 17:55:43 +0530

    Added home page

--- lib/Pages/Homepage.dart
+++ lib/Pages/Homepage.dart
@@ -1,30 +1,198 @@
+import 'package:book_management/Other/List.dart';
 import 'package:flutter/material.dart';
 import 'package:book_management/Other/background.dart';
-
 class HomePage extends StatefulWidget{
   @override
-  State<StatefulWidget> createState() {
+  State<StatefulWidget> createState()
+  {
    return HomePageState();
   }
 }
 class HomePageState extends State<HomePage>{
+  bool first=false,second=false,third=false;
+  var firstColour= Colors.transparent,secondColour= Colors.transparent,thirdColour= Colors.transparent;
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return SafeArea(
       child: Scaffold(
-        body: Stack(
-          children: [
-            backGround(context),
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              children: [
+                backGround(context),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 19,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(width: 19,),
+                        Icon(Icons.menu,
+                        size: 30,
+                        color: Color(0XFF42210B),),
+                        SizedBox(width: 290,),
+                        Icon(Icons.search,
+                        size: 30,color: Color(0XFF42210B)),
+                      ],
+                    ),
+                    SizedBox(height:50),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          SizedBox(width: 10,),
+                          RaisedButton(
+                            onPressed: (){
+                            print("Button is pressed");
+                            setState(() {
+                              first=!first;
+                              if(first==true)
+                                {
+                                  firstColour=Color(0xFFFBB03B);
+                                  secondColour=Colors.transparent;
+                                  thirdColour=Colors.transparent;
+                                  second=false;
+                                  third=false;
+                                }
+                            });
+                          },
+                            child: new Text("POPULAR",
+                            style: TextStyle(fontSize: 19,color: Color(0xFF42210B)),),
+                            color: firstColour,
+                            elevation:0,
+                              shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(18.0),)
+                          ),
+                          SizedBox(width: 10,),
+                          RaisedButton(
+                            onPressed: (){
+                              print("Button is pressed");
+                              setState(() {
+                                second=!second;
+                                if(second==true)
+                                {
+                                  secondColour=Color(0xFFFBB03B);
+                                  firstColour=Colors.transparent;
+                                  thirdColour=Colors.transparent;
+                                  first=false;
+                                  third=false;
+                                }
+                              });
+                            },
+                            child: new Text("RECOMMENDED",
+                              style: TextStyle(fontSize: 19,color: Color(0xFF42210B)),),
+                            // color: Color(0xFFFBB03B),
+                              color: secondColour,
+                            elevation:0,
+                              shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(18.0),)
+                          ),
+                          SizedBox(width: 10,),
+                          RaisedButton(
+                            onPressed: (){
+                              print("Button is pressed");
+                              setState(() {
+                                third=!third;
+                                if(third==true)
+                                {
+                                  thirdColour=Color(0xFFFBB03B);
+                                  firstColour=Colors.transparent;
+                                  secondColour=Colors.transparent;
+                                  second=false;
+                                  first=false;
+                                }
+                              });
+                            },
+                            child: new Text("NEW RELEASES",
+                              style: TextStyle(fontSize: 19,color: Color(0xFF42210B)),),
+                            color:thirdColour,
+                            elevation:0,
+                              shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(18.0),)
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10.0,),
+            Container(
+              color: Colors.transparent,
+              width:MediaQuery.of(context).size.width,
+              height: 330,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: 10,
+                      itemBuilder: (BuildContext context, index){
+                        return Padding(
+                          padding: EdgeInsets.only(left: 40.0),
+                          child: Stack(
+                            overflow: Overflow.visible,
+                            children: [
+                              Container(
+                                height: 310,
+                                width: 240,
+                                decoration: BoxDecoration(
+                                  image: new DecorationImage(
+                                    image: new AssetImage("images/Asset1.png"),
+                                    fit: BoxFit.fill,
+                                  ),
+                                  // color:Color(0xFFFBB03B),
+                                  color: Colors.black87,
+                                  borderRadius: BorderRadius.circular(40),
+                                ),
+                              ),
+                              Positioned(
+                                  bottom: 55,
+                                  left: 205,
+                                  child: CircleAvatar(
+                                    backgroundColor: Color(0XFF42210B),
+                                    child: CircleAvatar(
+                                        radius: 28,
+                                        backgroundColor: Color(0XFFFBB03B),
+                                        child: Text(popularlist[index].price.toString() + "/-",
+                                          style: TextStyle(fontSize: 18,color: Color(0XFF42210B)),)),
+                                    radius: 30,
+                                  )),
+                              Positioned(
+                                  bottom: 0.5,
+                                  left: 0,
+                                  child:Container(
+                                    width: 240,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      // color:Color(0xFFFBB03B),
+                                      color: Color(0XFF8C6239),
+                                      borderRadius: BorderRadius.only(
+                                        bottomLeft: Radius.circular(40),
+                                        bottomRight: Radius.circular(40),
+                                      ),
+                                    ),
+                                  ))
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                    SizedBox(width: 40,)
+                  ],
+                ),
+              ),
+            ),
 
-          ],
+
+
+                 ],
+                )
+              ],
+            ),
+          ),
         )
       ),
     );
   }
 }
-
-
-
-
-
-
